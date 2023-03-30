64b06cfa64340e92be59fbd000fa4c419ac21721
Vlad Zaharia <hey@vlad.gg>
2023-03-29 22:24:21 -0700

    Move vault helpers to plugin

---
+++ dot_zsh_custom/plugins/vault-helper/helpers/vtoken.zsh
@@ -0,0 +1,18 @@
+# Lookup token information and capabilities
+function vtoken() {
+    if [ "$(command -v vault)" ]; then; else
+        echo "${_COLOR_RED}[!] vault must be installed to use vtoken!${_RESET}"
+        return 1
+    fi
+
+    if [ $# -eq 0 ]; then
+        echo "${_COLOR_RED}[!] Must provide token, optionally with path to check!${_RESET}"
+        return 1
+    fi
+
+    if [ $# -eq 1 ]; then
+	vault token lookup $1
+    elif [ $# -eq 2 ]; then
+	vault token capabilities $1 $2
+    fi
+}
---
+++ dot_zsh_custom/plugins/vault-helper/vault-helper.plugin.zsh
@@ -0,0 +1,8 @@
+_COLOR_RED='\033[1;31m'
+_COLOR_YELLOW='\033[1;33m'
+_RESET='\033[0m'
+
+source ${0:A:h}/helpers/vlogin.zsh
+source ${0:A:h}/helpers/votp.zsh
+source ${0:A:h}/helpers/vssh.zsh
+source ${0:A:h}/helpers/vtoken.zsh
