19b15fa00f7eaff9fc5f93d164f23e0b96b7f07b
Vlad Zaharia <hey@vlad.gg>
2023-03-29 22:40:31 -0700

    Clean up errors, add help

--- dot_zsh_custom/plugins/vault-helper/helpers/votp.zsh
+++ dot_zsh_custom/plugins/vault-helper/helpers/votp.zsh
@@ -1,17 +1,24 @@
 # Generate OTP code
 function votp() {
     if [ "$(command -v vault)" ]; then; else
-        echo "${_COLOR_RED}[!] vault must be installed to use votp!${_RESET}"
+        echo "${_COLOR_RED}[!]${_RESET} vault must be installed to use votp!"
         return 1
     fi
 
-    if [ $# -eq 0 ]; then
-        echo "${_COLOR_RED}[!] Must provide TOTP key!${_RESET}"
+    if [ $# -eq 0 ] || [[ $1 == -h* ]]; then
+        if [ $# -eq 0 ]; then
+            echo "${_COLOR_RED}[!]${_RESET} Must provide TOTP key!"
+            echo ""
+        fi
+        
+        echo "${_COLOR_CYAN}[i]${_RESET} votp [-l] <key>"
+        echo "${_COLOR_CYAN}[i]${_RESET} key: TOTP key to retrieve token for"
+        echo "${_COLOR_CYAN}[i]${_RESET}  -l: List all keys"
         return 1
     fi
 
     if [[ $1 == -l* ]]; then
-        echo "${_COLOR_YELLOW}[i] Listing tokens${_RESET}"
+        echo "${_COLOR_YELLOW}[!]${_RESET} -l passed in, listing keys"
             
         vault list totp/keys
     else
--- dot_zsh_custom/plugins/vault-helper/helpers/vssh.zsh
+++ dot_zsh_custom/plugins/vault-helper/helpers/vssh.zsh
@@ -1,12 +1,19 @@
 # SSH to remote machine using Vault SSH certificate
 function vssh() {
     if [ "$(command -v vault)" ]; then; else
-        echo "${_COLOR_RED}[!] vault must be installed to use vlogin!${_RESET}"
+        echo "${_COLOR_RED}[!]${_RESET} vault must be installed to use vssh!"
         return 1
     fi
 
-    if [ $# -eq 0 ]; then
-        echo "${_COLOR_RED}[!] Must provide hostname!${_RESET}"
+    if [ $# -eq 0 ] || [[ $1 == -h* ]]; then
+        if [ $# -eq 0 ]; then
+            echo "${_COLOR_RED}[!]${_RESET} Must provide hostname!"
+            echo ""
+        fi
+        
+        echo "${_COLOR_CYAN}[i]${_RESET} vssh [-r] <hostname>"
+        echo "${_COLOR_CYAN}[i]${_RESET} hostname: Computer to connect to"
+        echo "${_COLOR_CYAN}[i]${_RESET}       -r: Connect as root"
 	    return 1
     fi
 
@@ -16,7 +23,7 @@ function vssh() {
     host=$1
 
     if [[ $1 == -r* ]]; then
-        echo "${_COLOR_YELLOW}[i] Connecting as root${_RESET}"
+        echo "${_COLOR_YELLOW}[!]${_RESET} -r passed in, connecting as root"
             
         role=root
         username=root
