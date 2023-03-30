35d39bd3c13b95001e83b9aa3907aefe4be4e57a
Vlad Zaharia <hey@vlad.gg>
2023-03-29 23:37:42 -0700

    Add cleanup to vnv

--- dot_zsh_custom/plugins/vault-helper/helpers/vnv.zsh
+++ dot_zsh_custom/plugins/vault-helper/helpers/vnv.zsh
@@ -6,13 +6,9 @@ function vnv() {
     fi
 
     if [[ $1 == -h* ]]; then
-        if [ $# -eq 0 ]; then
-            echo "${_COLOR_RED}[!]${_RESET} Must provide project to inject!"
-            echo ""
-        fi
-        
-        echo "${_COLOR_CYAN}[i]${_RESET} vnv <project>"
+        echo "${_COLOR_CYAN}[i]${_RESET} vnv [-c] <project>"
         echo "${_COLOR_CYAN}[i]${_RESET} project: Project to get dotenv variables for"
+        echo "${_COLOR_CYAN}[i]${_RESET}      -c: Cleanup environment variables for project"
         return 1
     fi
 
@@ -22,13 +18,26 @@ function vnv() {
         echo "${_COLOR_YELLOW}[!]${_RESET} No project was specified, using current directory '$project'"
     fi
 
-    echo "${_COLOR_CYAN}[i]${_RESET} Injecting variables from dotenv/$project/dev"
+    if [[ $1 == -c* ]]; then
+        project=$2
+        echo "${_COLOR_YELLOW}[!]${_RESET} -c provided, cleaning environment variables for '$project'"
+    else
+        echo "${_COLOR_CYAN}[i]${_RESET} Injecting variables from dotenv/$project/dev"
+    fi
+
     local json_out=$(vault read -format=raw dotenv/$project/dev)
     local parsed=($(echo $json_out | jq -r '.data | to_entries | map(.key+"="+.value) | @sh'))
 
     for envvar in $parsed; do
         exported=$(echo $envvar | tr -d "'")
-        export $exported
-        echo "${_COLOR_GREEN}[✓]${_RESET} Finished injecting $(echo $exported | cut -d "=" -f 1)"
+        key=$(echo $exported | cut -d "=" -f 1)
+
+        if [[ $1 == -c* ]]; then
+            unset $key
+            echo "${_COLOR_GREEN}[✓]${_RESET} Finished cleaning up $key"
+        else
+            export $exported
+            echo "${_COLOR_GREEN}[✓]${_RESET} Finished injecting $key"
+        fi
     done
 }
