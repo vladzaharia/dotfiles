947015e5b054f7d564775e6cc6ec1d88d0ed1c80
Vlad Zaharia <v.zaharia@pokemon.com>
2023-03-31 22:43:47 -0700

    Clean up and document vault helpers

--- dot_zsh_custom/plugins/vault-helper/helpers/vlogin.zsh
+++ dot_zsh_custom/plugins/vault-helper/helpers/vlogin.zsh
@@ -1,10 +1,12 @@
 # Login to Vault
 function vlogin {
-    if [ "$(command -v vault)" ]; then; else
+    # Check that vault exists
+    if [ $(command -v vault) ]; then; else
         echo "${_COLOR_RED}[!]${_RESET} vault must be installed to use vlogin!"
         return 1
     fi
 
+    # Help text if -h is passed in
     if [[ $1 == -h* ]]; then
         echo "${_COLOR_CYAN}[i]${_RESET} vlogin [-t]"
         echo "${_COLOR_CYAN}[i]${_RESET} -t: Print out token"
@@ -13,9 +15,11 @@ function vlogin {
 
     args="-no-print"
     
+    # Print out token when -t is passed in
     if [[ $1 == -t* ]]; then            
         args="-field=token"
     fi
 
+    # Perform Vault OIDC login using Authentik
     vault login -method=oidc $args
 }
