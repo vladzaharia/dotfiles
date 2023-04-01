6948735c984af720553c080aadb636eed7263d75
Vlad Zaharia <hey@vlad.gg>
2023-03-31 22:17:46 -0700

    Add SSH key findeer

--- dot_zsh_custom/plugins/vault-helper/helpers/vssh.zsh
+++ dot_zsh_custom/plugins/vault-helper/helpers/vssh.zsh
@@ -10,8 +10,8 @@ function vssh() {
             echo "${_COLOR_RED}[!]${_RESET} Must provide hostname!"
             echo ""
         fi
-        
-        echo "${_COLOR_CYAN}[i]${_RESET} vssh [-r] <hostname>"
+
+        echo "${_COLOR_CYAN}[i]${_cRESET} vssh [-r] <hostname>"
         echo "${_COLOR_CYAN}[i]${_RESET} hostname: Computer to connect to"
         echo "${_COLOR_CYAN}[i]${_RESET}       -r: Connect as root"
 	    return 1
@@ -22,6 +22,10 @@ function vssh() {
     username=$USER
     host=$1
 
+    _vssh_find_key
+
+    echo "${_COLOR_CYAN}[i]${_RESET} Using public key $HOME/.ssh/$sshKey.pub"
+
     if [[ $1 == -r* ]]; then
         echo "${_COLOR_YELLOW}[!]${_RESET} -r passed in, connecting as root"
             
@@ -29,5 +33,15 @@ function vssh() {
         username=root
         host=$2
     fi
-    vault ssh -mode=$mode -role=$role $username@$host
+    vault ssh -mode=$mode -role=$role -public-key-path="$HOME/.ssh/$sshKey.pub" -private-key-path="$HOME/.ssh/$sshKey" $username@$host
+}
+
+function _vssh_find_key() {
+    possibleKeys=(id_rsa id_ecdsa id_ed25519 id_dsa id_pkmn)
+    for key in $possibleKeys; do
+        if [ -f "$HOME/.ssh/$key.pub" ]; then
+            sshKey=$key
+            return
+        fi
+    done
 }
