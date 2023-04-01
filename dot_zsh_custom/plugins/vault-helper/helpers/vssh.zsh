44a0c168d3f1c32e82c5b318964780574a9bbfc2
Vlad Zaharia <v.zaharia@pokemon.com>
2023-03-31 22:26:56 -0700

    Add documentation for username parameter

--- dot_zsh_custom/plugins/vault-helper/helpers/vssh.zsh
+++ dot_zsh_custom/plugins/vault-helper/helpers/vssh.zsh
@@ -13,6 +13,7 @@ function vssh() {
 
         echo "${_COLOR_CYAN}[i]${_cRESET} vssh [-r] [username@]<hostname>"
         echo "${_COLOR_CYAN}[i]${_RESET} hostname: Computer to connect to"
+        echo "${_COLOR_CYAN}[i]${_RESET} username: User to connect as, defaults to your own"
         echo "${_COLOR_CYAN}[i]${_RESET}       -r: Connect as root"
 	    return 1
     fi
